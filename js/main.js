2cb4d8af244de7b34818c1a8df78730799d83797
Your Name <samueldewaxi@gmail.com>
2021-12-01 21:38:34 +0000

    modified

--- js/main.js
+++ js/main.js
@@ -73,4 +73,10 @@ scrollLinks.forEach(getScrollKinks = (scroll) =>{
     })
 })
 
-
+ $(window).on('load', function () {
+    if ($('#preloader').length) {
+      $('#preloader').delay(100).fadeOut('slow', function () {
+        $(this).remove();
+      });
+    }
+  });
